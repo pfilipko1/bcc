2ce21b23799011e4e92f8524903e327beb3016c2
pfilipko1 <piotr.filipkowski@intel.com>
2022-09-20 19:43:10 +0200

    reverse

--- examples/cpp/pyperf/PyPerfNativeStackTrace.cc
+++ examples/cpp/pyperf/PyPerfNativeStackTrace.cc
@@ -12,7 +12,6 @@
 #include <cstdio>
 #include <cstring>
 #include <sstream>
-#include <cxxabi.h>
 
 #include "PyPerfLoggingHelper.h"
 
@@ -68,15 +67,11 @@ NativeStackTrace::NativeStackTrace(uint32_t pid, const unsigned char *raw_stack,
   do {
     unw_word_t offset;
     char sym[256];
-    char   *realname;
-    int     status;
     // TODO: This function is very heavy. We should try to do some caching here, maybe in the
     //       underlying UPT function.
     res = unw_get_proc_name(&cursor, sym, sizeof(sym), &offset);
     if (res == 0) {
-      realname = abi::__cxa_demangle(sym, 0, 0, &status);
-      this->symbols.push_back(std::string(realname));
-      free(realname);
+      this->symbols.push_back(std::string(sym));
     } else {
       unw_word_t ip;
       unw_get_reg(&cursor, UNW_REG_IP, &ip);
